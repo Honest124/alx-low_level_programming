2b0461a7cbf36968591f94ff2a656ec114f6ff46
honest124 <honeststeven19@gmail.com>
2023-02-21 13:04:23 -0800

    Task 13. Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,154 +1,35 @@
+#include "main.h"
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * sum - print sum for multiples of 3 and 5 of natural numbers below 1024
  *
- * Return: Always 0 (Success)
+ * Return: 0 always
+ * Author: IanoNjuguna
  */
-int main(void)
+void sum(void)
 {
-	int i, sum = 0;
+	int i = 1024;
+	int sum = 0;
 
 	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 5) == 0 || (i % 3) == 0)
-			sum += i;
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum = sum + i;
+		}
 	}
 	printf("%d\n", sum);
+}
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	sum();
 	return (0);
-}#include <stdio.h>
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-int main(void)
-{
-		int i, sum = 0;
-
-			for (i = 0; i < 1024; i++)
-			{
-						if ((i % 5) == 0 || (i % 3) == 0)
-										sum += i;
-			}
-				printf("%d\n", sum);
-					return (0);
-}#include <stdio.h>
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-int main(void)
-{
-		int i, sum = 0;
-
-			for (i = 0; i < 1024; i++)
-			{
-						if ((i % 5) == 0 || (i % 3) == 0)
-										sum += i;
-			}
-				printf("%d\n", sum);
-					return (0);
-}#include <stdio.h>
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-int main(void)
-{
-		int i, sum = 0;
-
-			for (i = 0; i < 1024; i++)
-			{
-						if ((i % 5) == 0 || (i % 3) == 0)
-										sum += i;
-			}
-				printf("%d\n", sum);
-					return (0);
-}#include <stdio.h>
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-int main(void)
-{
-		int i, sum = 0;
-
-			for (i = 0; i < 1024; i++)
-			{
-						if ((i % 5) == 0 || (i % 3) == 0)
-										sum += i;
-			}
-				printf("%d\n", sum);
-					return (0);
-}#include <stdio.h>
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-int main(void)
-{
-		int i, sum = 0;
-
-			for (i = 0; i < 1024; i++)
-			{
-						if ((i % 5) == 0 || (i % 3) == 0)
-										sum += i;
-			}
-				printf("%d\n", sum);
-					return (0);
-}#include <stdio.h>
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-int main(void)
-{
-		int i, sum = 0;
-
-			for (i = 0; i < 1024; i++)
-			{
-						if ((i % 5) == 0 || (i % 3) == 0)
-										sum += i;
-			}
-				printf("%d\n", sum);
-					return (0);
-}#include <stdio.h>
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-int main(void)
-{
-		int i, sum = 0;
-
-			for (i = 0; i < 1024; i++)
-			{
-						if ((i % 5) == 0 || (i % 3) == 0)
-										sum += i;
-			}
-				printf("%d\n", sum);
-					return (0);
-}#include <stdio.h>
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-int main(void)
-{
-		int i, sum = 0;
-
-			for (i = 0; i < 1024; i++)
-			{
-						if ((i % 5) == 0 || (i % 3) == 0)
-										sum += i;
-			}
-				printf("%d\n", sum);
-					return (0);
 }
